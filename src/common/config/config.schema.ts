47c78f6fa83084a109df5b8195c6ffa6bbc68e52
Aleksandr Tarasyuk <alextarasyuk@fastmail.com>
2022-10-25 21:33:13 +0700

    JWT

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -12,6 +12,7 @@ export type ConfigSchema = {
   DB_PORT: number;
   DB_NAME: string;
   UPLOAD_DIRECTORY: string;
+  JWT_SECRET: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
@@ -60,6 +61,12 @@ export const configSchema = convict<ConfigSchema>({
     doc: 'Directory for upload files',
     format: String,
     env: 'UPLOAD_DIRECTORY',
+    default: null
+  },
+  JWT_SECRET: {
+    doc: 'Secret for sign JWT',
+    format: String,
+    env: 'JWT_SECRET',
     default: null
   }
 });
